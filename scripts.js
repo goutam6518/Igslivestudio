56a5d9b99bea569633dc952e54011d701dfc889d
FOGSTUDIO <gkfogatudio7@gmail.com>
2025-05-15 09:42:45 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-apiKey: "AIzaSyCRtPZ34Y1J-p5b7FJxEUagYg3h_D6PbhM",
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCRtPZ34Y1J-p5b7FJxEUagYg3h_D6PbhM",
     authDomain: "igsfogstudio-df541.firebaseapp.com",
     projectId: "igsfogstudio-df541",
     storageBucket: "igsfogstudio-df541.firebasestorage.app",
     messagingSenderId: "206722625476",
-    appId: "1:206722625476:web:9ab922dc4853418af57e91",
-    measurementId: "G-71VB7K6QS6"
-};
+    appId: "1:206722625476:web:c222830b5404f87bf57e91",
+    measurementId: "G-1JL82Z0FK0"
+  };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app););
+
 
 let userId = "";
 
